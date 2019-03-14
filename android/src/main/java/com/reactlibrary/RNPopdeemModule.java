1d2d510c6025ed4263b24e2102f731f24c6828f6
Hoa Vo <vovanhoait@gmail.com>
2019-03-15 03:20:56 +0700

    update activity handle for android

--- android/src/main/java/com/reactlibrary/RNPopdeemModule.java
+++ android/src/main/java/com/reactlibrary/RNPopdeemModule.java
@@ -2,6 +2,8 @@ package com.reactlibrary;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.popdeem.sdk.core.PopdeemSDK;
@@ -9,14 +11,26 @@ import com.popdeem.sdk.core.utils.PDLog;
 import com.popdeem.sdk.core.api.PDAPICallback;
 import com.popdeem.sdk.core.api.PDAPIClient;
 import com.popdeem.sdk.core.api.response.PDBasicResponse;
+import android.app.Activity;
+import android.content.Intent;
 
 public class RNPopdeemModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
+  private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
+      super.onActivityResult(requestCode, resultCode, intent);
+      //implement code here
+    }
+  };
+
   public RNPopdeemModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    reactContext.addActivityEventListener(mActivityEventListener);
   }
 
   @Override
